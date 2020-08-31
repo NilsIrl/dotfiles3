2eee4b16452a973a53a24bb547154a95dd69cb77
Nils <nils@nilsand.re>
2020-08-31 15:04:15 +0100

    [sway] Move get_volume to function
    There are also some improvements to the code

--- .config/sway/get_volume.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-pactl list sinks | grep no >/dev/null && pactl list sinks | grep '^[[:space:]]Volume:' | grep -o '[[:digit:]]*' | tail --lines +2 | head --lines 1 || echo 0
--- .config/sway/wob_volume.sh
+++ .config/sway/wob_volume.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-pactl subscribe | grep --line-buffered ' sink ' | while IFS= read -r; do volume=`~/.config/sway/get_volume.sh`; echo $(($volume > 100 ? 100 : $volume)); done | wob
+get_volume() {
+	pactl list sinks | grep 'Mute: no' >/dev/null && pactl list sinks | grep '^[[:space:]]Volume:' | grep -o '[[:digit:]]*' | tail --lines +2 | head --lines 1 || echo 0
+}
+
+pactl subscribe | grep --line-buffered ' sink ' | while IFS= read -r; do volume=$(get_volume); echo $((volume > 100 ? 100 : volume)); done | wob
